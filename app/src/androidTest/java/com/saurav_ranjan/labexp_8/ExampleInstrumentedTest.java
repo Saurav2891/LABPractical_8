19a3e9ed00325593b584a772d2c9da5286f9153e
Saurav Ranjan <ranjansaurav21@gmail.com>
2022-01-26 16:34:24 +0530

    Lab_Practical_8

---
+++ app/src/androidTest/java/com/saurav_ranjan/labexp_8/ExampleInstrumentedTest.java
@@ -0,0 +1,21 @@
+package com.saurav_ranjan.labexp_8;
+
+import android.content.Context;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals( "com.saurav_ranjan.labexp_8", appContext.getPackageName() );
+    }
+}
